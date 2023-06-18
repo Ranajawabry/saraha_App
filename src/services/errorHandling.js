dacc7cddc3f1294abace414caa2c95f5c8a7c86b
Ranajawabry <rana.a.jawabry@gmail.com>
2023-06-18 08:29:21 +0300

    new

--- src/services/errorHandling.js
+++ src/services/errorHandling.js
@@ -2,7 +2,7 @@ export const asyncHandler=(fn)=>{
 
     return (req,res)=>{
         fn(req,res).catch(error=>{
-            return res.status(500).json({message:"catching error" , error})
+            return res.status(500).json({message:"catching error" , error:error.stack})
         })
 
         }
